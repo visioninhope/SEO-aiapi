cfaab0b955ca9564099251a6683cbca6bfb4f29b
chaos <chaos29092@gmail.com>
2023-12-20 16:57:03 +0800

    文章搜索列表页接口返回的数据补上了type,create_date,source

--- src/articles/models.py
+++ src/articles/models.py
@@ -1,11 +1,16 @@
 # for db models
 import pymongo
 from beanie import Document, Indexed
-
+from typing import Optional
+from datetime import datetime
 
 class Article(Document):
     keyword: Indexed(str, index_type=pymongo.TEXT)
+    type: Optional[str] = None
+    source: Optional[str] = None
+    create_date: datetime = datetime.now()
     content: str
+
 
     class Settings:
         name = "articles"
